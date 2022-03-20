85d4abeaafb33b56624d8bc35bda0a3a37cca78a
Lucas <lucas.ar93@gmail.com>
2022-03-20 19:30:46 -0300

    add rule to eslintrc file, 'no-unused-vars: off'

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,27 @@
+import React from "react";
+import image from "../../assets/images/plain-logo.png";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  margin: 0 auto;
+  width: 100vw;
+  overflow-x: hidden;
+  align-self: center;
+  background-color: black;
+`;
+
+const Image = styled.img`
+  /* margin: 0 auto; */
+  padding: 5px;
+`;
 function Header() {
-  return <>LOGO</>;
+  return (
+    <StyledHeader>
+      <Image src={image} alt="webuddies logo" />
+    </StyledHeader>
+  );
 }
 
 export default Header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Header from "./components/Header/Header.js";
+import "./styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
