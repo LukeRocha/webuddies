f1c1b3a153bd89df47a817b43c1353b7a1bbbb03
Lucas <lucas.ar93@gmail.com>
2022-03-20 20:38:17 -0300

    add header with styled components and material ui lib

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,27 +1,64 @@
-import React from "react";
-import image from "../../assets/images/plain-logo.png";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import DensityMediumIcon from "@mui/icons-material/DensityMedium";
 import styled from "styled-components";
 
 const StyledHeader = styled.header`
   display: flex;
-  flex-direction: row;
-  margin: 0 auto;
-  width: 100vw;
-  overflow-x: hidden;
-  align-self: center;
-  background-color: black;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  alignt-content: center;
+  background: #242424;
+  padding: 10px;
+  text-align: center;
 `;
 
-const Image = styled.img`
-  /* margin: 0 auto; */
-  padding: 5px;
+const HeaderLogo = styled.h2`
+  margin: 0 auto;
+  color: white;
+  align-self: center;
+  position: relative;
 `;
-function Header() {
+
+export default function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <StyledHeader>
-      <Image src={image} alt="webuddies logo" />
-    </StyledHeader>
+    <div>
+      <StyledHeader>
+        <HeaderLogo>weBuddies</HeaderLogo>
+        <Button
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <DensityMediumIcon />
+        </Button>
+      </StyledHeader>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
   );
 }
-
-export default Header;
