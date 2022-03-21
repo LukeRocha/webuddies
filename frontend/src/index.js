b5fb2fd6d4fac5e393369079281a22ab737c7b7f
Lucas <lucas.ar93@gmail.com>
2022-03-21 13:01:49 -0300

    Finish main design of mobile nav bottom component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Header from "./components/Header/Header.js";
+import BottomNav from "./components/BottomNav/BottomNav.js";
 import "./styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <App />
+    <BottomNav />
   </React.StrictMode>,
   document.getElementById("root")
 );
