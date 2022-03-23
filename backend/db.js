206c2f0fb08a490fa9ec1d4faaccf26a860d9a65
Lucas <lucas.ar93@gmail.com>
2022-03-23 13:13:11 -0300

    Knex library setup

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  user: "lucas",
-  host: "127.0.0.1",
-  database: "postgres",
-  password: "postgres",
-  port: 5432,
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+    database: "webuddies",
+  },
 });
 
-console.log(pool);
+module.exports = knex;
