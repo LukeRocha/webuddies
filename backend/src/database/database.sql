5a978f2c3dea9fb0aa6750114c2f3c90793c9172
Lucas <lucas.ar93@gmail.com>
2022-03-28 11:51:52 -0300

    change column names in database.sql

--- backend/src/database/database.sql
+++ backend/src/database/database.sql
@@ -1,29 +1,29 @@
 CREATE DATABASE IF NOT EXISTS webuddies
 
-CREATE TABLE IF NOT EXISTS users_table(
+CREATE TABLE IF NOT EXISTS users(
   id SERIAL PRIMARY KEY,
   nickname  VARCHAR(15) UNIQUE NOT NULL,
-  firstname VARCHAR(20) NOT NULL,
-  lastname  VARCHAR(20) NOT NULL,
+  first_name VARCHAR(20) NOT NULL,
+  last_name  VARCHAR(20) NOT NULL,
   birth     DATE NOT NULL,
   city      VARCHAR(50) NOT NULL,
   country   VARCHAR(56) NOT NULL,
   mail      VARCHAR(50) NOT NULL,
   phone     VARCHAR(11) NOT NULL
-  userpassword  VARCHAR(25) NOT NULL
-  profilepicture  VARCHAR(255),
-  userstatus VARCHAR(255),
-  isdeleted BOOLEAN NOT NULL
+  password  VARCHAR(25) NOT NULL
+  profile_picture  VARCHAR(255),
+  user_status VARCHAR(255),
+  is_deleted BOOLEAN NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS friendships (
-  main_userid FOREIGN KEY NOT NULL,
-  target_friendid FOREIGN KEY
+  main_user_id FOREIGN KEY NOT NULL,
+  target_friend_id FOREIGN KEY
 )
 
-CREATE TABLE IF NOT EXISTS posts_table (
+CREATE TABLE IF NOT EXISTS posts (
   id SERIAL PRIMARY KEY,
-  userid FOREIGN KEY,
-  ispinned BOOLEAN NOT NULL,
-  postcontent VARCHAR(320)
+  user_id FOREIGN KEY,
+  is_pinned BOOLEAN NOT NULL,
+  post_content VARCHAR(320)
 )
