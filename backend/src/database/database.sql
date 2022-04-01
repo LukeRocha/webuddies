0cc8188d247fc7232b22b2e4d95160cd1a524f29
Lucas <lucas.ar93@gmail.com>
2022-03-31 23:34:04 -0300

    Adjustments on database.sql table creation

--- backend/src/database/database.sql
+++ backend/src/database/database.sql
@@ -9,21 +9,21 @@ CREATE TABLE IF NOT EXISTS users(
   city      VARCHAR(50) NOT NULL,
   country   VARCHAR(56) NOT NULL,
   mail      VARCHAR(50) NOT NULL,
-  phone     VARCHAR(11) NOT NULL
-  password  VARCHAR(25) NOT NULL
+  phone     VARCHAR(11) NOT NULL,
+  password  VARCHAR(25) NOT NULL,
   profile_picture  VARCHAR(255),
   user_status VARCHAR(255),
   is_deleted BOOLEAN NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS friendships (
-  main_user_id FOREIGN KEY NOT NULL,
-  target_friend_id FOREIGN KEY
+  main_user_id REFERENCES users (id),
+  target_friend_id REFERENCES users (id)
 )
 
 CREATE TABLE IF NOT EXISTS posts (
   id SERIAL PRIMARY KEY,
-  user_id FOREIGN KEY,
+  user_id INTEGER REFERENCES users (id),
   is_pinned BOOLEAN NOT NULL,
   post_content VARCHAR(320)
 )
