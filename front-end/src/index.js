9e633ee6b2edc215643a4bdb0f47d6821f3d0afc
lucas <lucas.ar93@gmail.com>
2022-06-14 01:06:19 -0300

    Add routes and links to components

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./context";
 import "./styles.css";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
 
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </AppProvider>
   </React.StrictMode>,
   document.getElementById("root")
