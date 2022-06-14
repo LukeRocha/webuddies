d1549b5465cdf97fcd3b4d5e444dc3d6a65cceee
lucas <lucas.ar93@gmail.com>
2022-06-14 11:58:02 -0300

    Add function to retrieve user data from database

--- front-end/src/operations/operations.js
+++ front-end/src/operations/operations.js
@@ -1,6 +1,20 @@
 import axios from "axios";
 const urls = {
   post: "http://localhost:3700/register",
+  login: "http://localhost:3700/login",
+};
+
+export const getUser = async () => {
+  const retrieveData = await axios
+    .get(urls.login)
+    .then((resp) => {
+      return resp;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(retrieveData);
+  return retrieveData;
 };
 
 export const postUser = async (userInputs) => {
