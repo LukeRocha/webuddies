a8c95e53922b92a153153f8eb98ac4881ecdfdd7
lucas <lucas.ar93@gmail.com>
2022-06-17 20:01:26 -0300

    Add post area component

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -4,12 +4,13 @@ import BottomNav from "../components/BottomNav/BottomNav";
 import Header from "../components/Header/Header";
 import UserProfile from "../components/User/UserProfile/UserProfile";
 import PostsContainer from "../components/PostsContainer/PostsContainer";
-
+import PostArea from "../components/PostArea/PostArea";
 const Profile = () => {
   return (
     <>
       <Header />
       <UserProfile />
+      <PostArea />
       <PostsContainer />
       <BottomNav />
     </>
