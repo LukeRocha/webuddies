647b3646fa8671f64e5020b917eba4c4b150d66e
lucas <lucas.ar93@gmail.com>
2022-06-18 22:56:16 -0300

    Remove margin property that was stated on Button component

--- front-end/src/components/Button/Button.js
+++ front-end/src/components/Button/Button.js
@@ -5,12 +5,11 @@ const StyledButton = styled.button`
   min-width: 220px;
   max-width: 40vw;
   padding: 5px;
-  margin: 0 auto;
   align-self: center;
   border-radius: 4px;
   border: none;
   color: #fff;
-
+  align-self: right;
   background-color: ${({ bg }) => (bg ? bg : "#000")};
 `;
 
