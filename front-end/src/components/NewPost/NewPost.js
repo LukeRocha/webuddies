2e14926285bac3a8df704d59d8429afe2f72a8d1
lucas <lucas.ar93@gmail.com>
2022-09-08 00:05:40 -0300

    Minor adjustments

--- front-end/src/components/NewPost/NewPost.js
+++ front-end/src/components/NewPost/NewPost.js
@@ -18,6 +18,7 @@ const PostArea = ({ children }) => {
   });
 
   useEffect(() => {
+    // getUserPostsData(accessToken);
     setPostContent({ ...postContent, user_id: userState.userData.id });
   }, [postContent.content]);
 
@@ -30,6 +31,15 @@ const PostArea = ({ children }) => {
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8czzbrLzXJ9R_uhKyMiwj1iGxKhJtH7pwlQ&usqp=CAU"
           />
         </ImageContainer>
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            createNewPost(postContent, accessToken);
+            setPostContent({ ...postContent, content: "" });
+            getUserPostsData(accessToken);
+          }}
+        />
         <PostContent
           maxLength={200}
           placeholder="Share something with buddies..."
@@ -44,10 +54,11 @@ const PostArea = ({ children }) => {
       <ButtonsWrapper>
         <>
           <Button
+            type="submit"
             bg={"var(--green-button)"}
-            onClick={() => {
+            onClick={(e) => {
+              e.preventDefault();
               createNewPost(postContent, accessToken);
-              console.log(postContent);
               setPostContent({ ...postContent, content: "" });
               getUserPostsData(accessToken);
             }}
--- front-end/src/components/PostsContainer/PostsContainer.js
+++ front-end/src/components/PostsContainer/PostsContainer.js
@@ -1,22 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import SinglePost from "../SinglePost/SinglePost";
 import { useGlobalContext } from "../../context";
 import { PostContainer } from "./styles";
 
 const PostsContainer = () => {
   const { accessToken, userState, getUserPostsData } = useGlobalContext();
+  const [userPosts, setUserPosts] = useState([]);
+
+  const renderPostsRealTimeForRealThisTime = useCallback(() => {
+    setUserPosts(userState.userPosts);
+    console.log(userPosts);
+  }, [userState.userPosts]);
 
   useEffect(() => {
     accessToken && getUserPostsData(accessToken);
+    renderPostsRealTimeForRealThisTime();
   }, [accessToken]);
 
   return (
-    <PostContainer>
-      {userState.userPosts &&
-        userState.userPosts.map((post) => {
-          return <SinglePost key={post.id} id={post.id} post={post} />;
-        })}
-    </PostContainer>
+    <>
+      <PostContainer renderPosts={renderPostsRealTimeForRealThisTime}>
+        {userPosts &&
+          userPosts.map((post) => {
+            return <SinglePost key={post.id} id={post.id} post={post} />;
+          })}
+      </PostContainer>
+      <PostContainer renderPosts={renderPostsRealTimeForRealThisTime}>
+        {userState.userPosts &&
+          userState.userPosts.map((post) => {
+            return <SinglePost key={post.id} id={post.id} post={post} />;
+          })}
+      </PostContainer>
+    </>
   );
 };
 
