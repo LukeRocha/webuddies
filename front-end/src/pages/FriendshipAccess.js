2bb21b74880409fa7a5bb20d0157956896b5466b
lucas <lucas.ar93@gmail.com>
2022-12-06 23:12:23 -0300

    Create component FriendshipProfile to render itself on FriendshipAccess page

---
+++ front-end/src/pages/FriendshipAccess.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import BottomNav from "../components/BottomNav/BottomNav";
+import FriendUser from "../components/User/FriendshipProfile/FriendshipProfile";
+
+const FriendshipAccess = () => {
+  return (
+    <>
+      <Header />
+      <FriendUser />
+      <BottomNav />
+    </>
+  );
+};
+
+export default FriendshipAccess;
