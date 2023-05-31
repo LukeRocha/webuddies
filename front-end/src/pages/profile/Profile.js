dfe48e7845778a1e954c63039423041840217827
Lucas <lucas.ar93@gmail.com>
2023-05-30 23:27:34 -0300

    Minor adjustment on profile page component import

--- front-end/src/pages/profile/Profile.js
+++ front-end/src/pages/profile/Profile.js
@@ -2,7 +2,7 @@ import React from "react";
 import BottomNav from "../../components/BottomNav/BottomNav";
 
 import Header from "../../components/Header/Header";
-import UserProfile from "../../components/User/UserProfile/UserProfile";
+import UserProfile from "../../components/User/UserProfile";
 import PostsContainer from "../../components/PostsContainer/PostsContainer";
 import NewPost from "../../components/NewPost/NewPost";
 
